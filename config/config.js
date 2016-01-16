f9c115975ef36eec0b7eb48de7e1451793acfa6f
koloff <kolof.anton@gmail.com>
2016-01-16 09:54:40 +0200

    changed scraper token

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ let env = process.env || 'development';
 
 let config = {};
 
-config.diffbotToken = '1de74bd0e4d1733fc7c343c58746127c';
+config.diffbotToken = '8165c3f96112eafc42acabd7046fef0c';
 
 config.port = env.port || 3377;
 config.db = env.db || 'mongodb://localhost/codestairs';
