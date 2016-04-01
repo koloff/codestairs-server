390fbd7678b6dcbac087a8ad3932d427135c027d
koloff <koloff.anton@gmail.com>
2016-04-01 20:47:25 +0300

    changed config

--- config/config.js
+++ config/config.js
@@ -1,15 +1,30 @@
 'use strict';
 let path = require('path');
-let env = process.env || 'development';
+let env = process.env.NODE_ENV || 'production';
+
+console.log(env);
+
+let settings;
+if (env === 'development') {
+  settings ={
+    "diffbotToken": "75ed683d89300b4b7eb73ff7e877e715",
+    "port": 3377,
+    "db": "mongodb://localhost/codestairs",
+    "secret": "johncena"
+  }
+} else {
+  settings = require('./SETTINGS.json');
+}
 
 let config = {};
 
-config.diffbotToken = '75ed683d89300b4b7eb73ff7e877e715';
-
-config.port = env.port || 3377;
-config.db = env.db || 'mongodb://localhost/codestairs';
-config.secret = env.secret || 'johncena';
 config.generatedDir = path.resolve(__dirname + './../generated');
+
+config.diffbotToken = settings.diffbotToken;
+
+config.port = env.port || settings.port;
+config.db = env.db || settings.db;
+config.secret = env.secret || settings.secret;
 
 
 module.exports = config;
